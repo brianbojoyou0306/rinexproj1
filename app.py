d028130395ea4e9451e238bd7bfb03e4840ac0e3
brianbojoyou0306 <108232870+brianbojoyou0306@users.noreply.github.com>
2022-07-03 20:52:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+model = joblib.load('gender')
+import numpy as np 
+st.title('GENDER CLASSIFIER')
+ip1 = st.number_input('Enter the height')
+ip2 = st.number_input('enter the weight')
+ls=np.append(ip1,ip2)
+op = model.predict([ls])
+if st.button('predict'):
+  st.title(op[0])
